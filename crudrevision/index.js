ffabb96ecc21dc15b7d2d31bcddd43f246ba14ce
rwn3developer <rwn3developer11@gmail.com>
2024-01-03 18:46:41 +0530

    crud-file-upload-delete-view

--- crudrevision/index.js
+++ crudrevision/index.js
@@ -4,7 +4,11 @@ const port = 8000;
 
 const app = express();
 
+const fs = require('fs');
+
 app.set('view engine','ejs');
+
+const multer = require('multer');
 
 const db = require('./config/db');
 
@@ -12,6 +16,21 @@ const User = require('./models/userModel');
 
 app.use(express.urlencoded());
 
+const path = require('path');
+
+app.use('/uploads',express.static(path.join('uploads')))
+
+//file upload by multer
+const fileUpload = multer.diskStorage({
+    destination : (req,file,cb) => {
+        cb(null,'uploads');
+    },
+    filename : (req,file,cb) => {
+        cb(null, file.originalname);
+    }
+})
+const imageUpload = multer({storage : fileUpload}).single('avatar');
+//file upload by multer end
 
 //route , http request , url
 app.get('/',(req,res)=>{
@@ -29,14 +48,15 @@ app.get('/add',(req,res)=>{
     return res.render('form');
 })
 
-app.post('/addRecord',(req,res)=>{
+app.post('/addRecord',imageUpload,(req,res)=>{
     const {name,email,password,gender,hobby,city,phone} = req.body;
-    if(!name || !email || !password || !gender || !hobby || !city || !phone){
+    if(!name || !email || !password || !gender || !hobby || !city || !phone || !req.file ){
         console.log("All field are required");
         return res.redirect('back');
     }
     User.create({
-        name,email,password,gender,hobby,city,phone
+        name,email,password,gender,hobby,city,phone,
+        image : req.file.path
     }).then((success)=>{
         console.log("User successfully insert");
         return res.redirect('/')
@@ -48,6 +68,15 @@ app.post('/addRecord',(req,res)=>{
 
 app.get('/deleteRecord',(req,res)=>{
     let id = req.query.id;
+    //image unlink
+    User.findById(id)
+    .then((oldRecord)=>{
+        fs.unlinkSync(oldRecord.image);
+    }).catch((err)=>{
+        console.log(err);
+        return false;
+    })
+    //mongodb record delete
     User.findByIdAndDelete(id)
     .then((success)=>{
         console.log("User delete");
--- crudrevision/models/userModel.js
+++ crudrevision/models/userModel.js
@@ -28,6 +28,10 @@ const userSchema = mongoose.Schema({
         type : String,
         required : true 
     },
+    image : {
+        type : String,
+        required : true
+    }
 });
 const user = mongoose.model('user',userSchema);
 module.exports = user;
