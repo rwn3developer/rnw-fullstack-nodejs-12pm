522234d78fd466375a5cf3e62de89c4211559f87
rwn3developer <rwn3developer11@gmail.com>
2024-01-16 18:52:52 +0530

    menual-auth-using-cookie

--- seprate-header-footerpage/index.js
+++ seprate-header-footerpage/index.js
@@ -5,11 +5,32 @@ const port = 8000;
 
 const app = express();
 
+const db = require('./config/db');
+
+const User = require('./models/userModel');
+
+const cookieParser = require('cookie-parser');
+
 app.use(express.static(path.join((__dirname,'public'))));
 
 app.set('view engine','ejs');
 
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    return res.render('index');
+})
+
+app.get('/register',(req,res)=>{
+    return res.render('register');
+})
+
 app.get('/dashboard',(req,res)=>{
+    if(!req.cookies['user']){
+        return res.redirect('/');
+    }
     return res.render('dashboard');
 })
 
@@ -19,6 +40,49 @@ app.get('/charts',(req,res)=>{
 
 app.get('/widgets',(req,res)=>{
     return res.render('widgets');
+})
+
+app.post('/registerUser',async(req,res)=>{
+        try{
+            let name = req.body.name;
+            let email = req.body.email;
+            let password = req.body.password;
+            let cpassword = req.body.cpassword;
+
+            if(cpassword == password){
+                let user = await User.create({
+                    name : name,
+                    email : email,
+                    password : password
+                });
+                console.log("User register");
+                return res.redirect('/');   
+            }else{
+                console.log("Check your confirm password");
+                return res.redirect('back');
+            }
+
+        }catch(err){
+            console.log(err);
+            return false;
+        }
+})
+
+app.post('/loginUser',async(req,res)=>{
+    try{
+        let email = req.body.email;
+        let password = req.body.password;
+        let user = await User.findOne({email : email});
+        if(!user || user.password != password){
+            console.log("Email and password are wrong");
+            return res.redirect('back');
+        }
+        res.cookie('user',user);
+        return res.redirect('/dashboard');
+    }catch(err){
+        console.log(err);
+        return false;
+    }
 })
 
 app.listen(port,(err)=>{
